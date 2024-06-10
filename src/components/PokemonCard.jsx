439ea864fdf8aebb31f182447f455e53c4186fd0
Mokalbari <150693283+Mokalbari@users.noreply.github.com>
2024-06-10 21:02:43 +0200

    add ternary operator to evaluate imgSrc presence in pokemonList

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -2,50 +2,29 @@ import React, { useState, useEffect } from 'react'
 import './PokemonCard.modules.css'
 
 const PokemonCard = () => {
-  const [averageColor, setAverageColor] = useState('')
-
-  useEffect(() => {
-    const img = new Image()
-    img.crossOrigin = 'Anonymous'
-    img.src =
-      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png'
-
-    img.onload = () => {
-      const canvas = document.createElement('canvas')
-      const ctx = canvas.getContext('2d')
-      canvas.width = img.width
-      canvas.height = img.height
-      ctx.drawImage(img, 0, 0)
-
-      const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
-      const data = imageData.data
-      let r = 0
-      let g = 0
-      let b = 0
-
-      for (let i = 0; i < data.length; i += 4) {
-        r += data[i] // Rouge
-        g += data[i + 1] // Vert
-        b += data[i + 2] // Bleu
-      }
-
-      const pixelCount = data.length / 4
-      r = Math.floor(r / pixelCount)
-      g = Math.floor(g / pixelCount)
-      b = Math.floor(b / pixelCount)
-
-      const averageColor = `rgb(${r}, ${g}, ${b})`
-      setAverageColor(averageColor)
-    }
-  }, [])
+  const pokemonList = [
+    {
+      name: 'bulbasaur',
+      imgSrc:
+        'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png',
+    },
+    {
+      name: 'mew',
+    },
+  ]
+  const pokemon = pokemonList[0]
 
   return (
-    <figure className="container" style={{ backgroundColor: averageColor }}>
-      <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        alt="Bulbasaur"
-      />
-      <figcaption>Bulbasaur</figcaption>
+    <figure className="container">
+      {pokemon.imgSrc ? (
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      ) : (
+        <p>???</p>
+      )}
+      <figcaption>
+        {pokemon.name.charAt(0).toUpperCase() +
+          pokemon.name.slice(1).toLowerCase()}
+      </figcaption>
     </figure>
   )
 }
