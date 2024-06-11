4c079e766b9982a34dd17667c8ccc1f2793f5911
Mokalbari <150693283+Mokalbari@users.noreply.github.com>
2024-06-11 09:36:14 +0200

    add props to PokemonCard

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,19 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import './PokemonCard.modules.css'
 
-const pokemonList = [
-  {
-    name: 'bulbasaur',
-    imgSrc:
-      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png',
-  },
-  {
-    name: 'mew',
-  },
-]
-
-const PokemonCard = () => {
-  const pokemon = pokemonList[0]
+const PokemonCard = ({ pokemon }) => {
   return (
     <figure className="container">
       {pokemon.imgSrc ? (
